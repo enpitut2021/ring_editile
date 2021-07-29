be4fec8eb52f6512455bc50223d99c1ed5032dd8
nomke <keita.nomura.228@gmail.com>
2021-07-29 10:27:57 +0900

    update login.dart

---
+++ lib/page/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('login page'),
+      ),
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}

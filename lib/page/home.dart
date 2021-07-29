f1d46e3242559ac00cac8c3c2389eeeaca80f1e7
M-Masayoshi <ma19ma26@gmail.com>
2021-07-29 18:36:59 +0900

    WIP:NewHome

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 import 'chat.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Chat',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   String privateID = "";
   Widget build(BuildContext context) {
@@ -33,6 +58,21 @@ class Home extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.cyan[400],
+        onTap: _onItemTapped,
       ),
     );
   }

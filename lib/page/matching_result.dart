5c58234a45d7cf0985be2eaa79402ee6f2a00c91
iFQinOvO <ifqinif@gmail.com>
2021-08-02 10:11:06 +0900

    matching_result_autojump

--- lib/page/matching_result.dart
+++ lib/page/matching_result.dart
@@ -16,6 +16,29 @@ class Match_Result extends StatefulWidget{
 }
 
 class _Match_Result extends State<Match_Result>{
+  int sec=5;
+  String infomsg = "秒後チャットルームに入ります";
+  //bool match_res=false;
+
+  void initState(){
+    nextpage();
+  }
+
+  void nextpage() async{
+    while(sec>0){
+      await Future.delayed(Duration(milliseconds: 1000));
+      setState(() {
+        sec -= 1;
+      });
+    }
+    
+    print("matching success");
+      Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ChatDemo("public",widget.auth)),
+                );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,6 +55,8 @@ class _Match_Result extends State<Match_Result>{
               'OOさんとマッチングしました',
               style: Theme.of(context).textTheme.headline6,
             ),
+
+            Text(sec.toString()+infomsg),
             
             //Divider(),
             // Indeterminate
@@ -45,15 +70,8 @@ class _Match_Result extends State<Match_Result>{
                 );
               },
             ),
-            RaisedButton(
-              child: Text('public chatroomに入る'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ChatDemo("public",widget.auth)),
-                );
-              },
-            ),
+            
+            
            
           ],
         ),

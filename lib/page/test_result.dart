f82353f700249ef1f16816626bf1aa7116e49aec
p0x0q <info.allegation@gmail.com>
2021-10-15 16:23:16 +0900

    WIP

--- lib/page/test_result.dart
+++ lib/page/test_result.dart
@@ -9,63 +9,63 @@ class test_result extends StatefulWidget {
   test_result(this.auth, this.name);
   Auth auth;
   String name;
-  
-
 
   @override
   State<StatefulWidget> createState() => _testresult();
 }
 
 class _testresult extends State<test_result> {
-List<Widget> post_test = [];
-Color _iconcolor= Colors.black;
-bool Press=false;
+  List<Widget> post_test = [];
+  Color _iconcolor = Colors.black;
+  bool Press = false;
 
   @override
-  void initState(){
+  void initState() {
     AccountAPI a = new AccountAPI(widget.auth.getBearer());
-                  a.getUserPostList();
-    // post_test.insert(0, 
-    // Column(children: [
-    //   Row(
-    //     children: [
-    //       Text(widget.name),
-    //     ]
-    //   ),
-    // ],));
-    
-    
-    
+    a.getUserPostList().then((posts){
+      posts.forEach((post) {
+        post_test.insert(0,
+        Column(children: [
+          Row(
+            children: [
+              Text(post.text),
+            ]
+          ),
+        ],));
+        });
+    });
   }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Column(
+      body: Column(children: <Widget>[
+        Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[Text(widget.name)],
-          
         ),
         Container(
-              height: 600,
-              width: 400,
-              child: ListView.builder(
-                itemCount: post_test.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return post_test[index];
+          height: 600,
+          width: 400,
+          child: ListView.builder(
+            itemCount: post_test.length,
+            itemBuilder: (BuildContext context, int index) {
+              return post_test[index];
+            },
+          ),
+        ),
+        Column(
+          children: [
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    this.Press = !this.Press;
+                    this._iconcolor = this.Press ? Colors.red : Colors.black;
+                  });
                 },
-              ),
-            ),
-          Column(children: [
-            IconButton(onPressed: (){
-            setState(() {
-              this.Press=!this.Press;
-              this._iconcolor=this.Press? Colors.red:Colors.black;
-            });
-          }, icon: Icon(Icons.favorite,color: this._iconcolor)
-          )],
-          )
-        ]),
+                icon: Icon(Icons.favorite, color: this._iconcolor))
+          ],
+        )
+      ]),
     );
   }
 }

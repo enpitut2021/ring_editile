a4697fa5582913598ce57329489a69d94367533e
p0x0q <p0x0q@users.noreply.github.com>
2022-01-07 07:07:16 +0000

    deploy: def0b038eb80516feb2fad6180c79d8beea699b4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,8 +20,8 @@ const RESOURCES = {
 "assets/AssetManifest.json": "3dd347f1770240ed9e3b717deadee690",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/assets/icon/ring-icon-color.png": "e3c14c113ef19e431380bdde00e5b64f",
-"index.html": "bc9674c31bb09a0956526ce825b97907",
-"/": "bc9674c31bb09a0956526ce825b97907",
+"index.html": "e5c22db5c9300db0e1c7d06da095b994",
+"/": "e5c22db5c9300db0e1c7d06da095b994",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };

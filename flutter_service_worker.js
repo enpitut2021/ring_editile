10b780fdb35b637f1c068b56128202635429de08
p0x0q <p0x0q@users.noreply.github.com>
2022-01-09 19:55:40 +0000

    deploy: c16a86ef79d96523c91a2a428e31d560cf3b7f3e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "aeff1b974dd64df3e3640a8c5b6a2e79",
+  "main.dart.js": "8299b4c0578da1c538584589be331bda",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "6721cea5902b9b6b2f71fbc6cbc63c61",
 "version.json": "08908a9d333232aef6aca5058d837d7d",
@@ -16,8 +16,8 @@ const RESOURCES = {
 "assets/AssetManifest.json": "3dd347f1770240ed9e3b717deadee690",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/assets/icon/ring-icon-color.png": "e3c14c113ef19e431380bdde00e5b64f",
-"index.html": "4421e6b2f261103eab7811e22bb4db5d",
-"/": "4421e6b2f261103eab7811e22bb4db5d",
+"index.html": "71c72b24b98e5cbee422207bc8ee24bb",
+"/": "71c72b24b98e5cbee422207bc8ee24bb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };

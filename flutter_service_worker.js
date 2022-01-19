294e4f5e32aff86abe96f269399b5d3f2c183196
nomke <nomke@users.noreply.github.com>
2022-01-19 04:56:40 +0000

    deploy: e462361bdb4aa165423f04e8a2f7765e185e4271

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "version.json": "08908a9d333232aef6aca5058d837d7d",
-"main.dart.js": "dc64e60b55e77ee44932df71527e9482",
+"main.dart.js": "e07a6cef6e3e96fdc14c1004273c57ce",
 "manifest.json": "6721cea5902b9b6b2f71fbc6cbc63c61",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/assets/icon/ring-icon-color.png": "e3c14c113ef19e431380bdde00e5b64f",
@@ -18,8 +18,8 @@ const RESOURCES = {
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "aa1ec80f1b30a51d64c72f669c1326a7",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"index.html": "29acdd4defadf46f5d2929a3c90ae417",
-"/": "29acdd4defadf46f5d2929a3c90ae417"
+"index.html": "89e97dfb491f11425e51e332a25ea7ff",
+"/": "89e97dfb491f11425e51e332a25ea7ff"
 };
 
 // The application shell files that are downloaded before a service worker can
